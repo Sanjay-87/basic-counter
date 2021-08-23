8c89f7c302cd51b5a19d77d545608d73d9350c96
SANJAY <sanjay@fisdom.com>
2021-08-24 00:59:06 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCounter = (data) => {
+    if (data === 'inc') setCount(count + 1)
+    else if (data === 'dec') setCount(count - 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={() => handleCounter('dec')}>
+        <Text style={styles.buttonText}>-</Text>
+      </TouchableOpacity>
+      <View style={styles.counter}>
+        <Text style={styles.number}>{count}</Text>
+      </View>
+      <TouchableOpacity style={styles.button}  onPress={() => handleCounter('inc')}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
   },
+  button: {
+    backgroundColor: 'lightblue',
+    height: 60,
+    width: 60,
+    textAlign: 'center',
+    borderRadius: '50%'
+  },
+  buttonText: {
+    fontSize: 40,
+    fontWeight: 600,
+    lineHeight: 60
+  },
+  counter: {
+    border: '1px solid',
+    height: 60,
+    width: 150,
+    marginHorizontal: 20,
+    textAlign: 'center'
+  },
+  number: {
+    lineHeight: 60,
+    fontSize: 25
+  }
 });
